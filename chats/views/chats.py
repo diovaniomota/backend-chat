1be84d646ef13dce659f371ecaf62ece36636803
Diovanio Mota <diovaniomota8@gmail.com>
2024-06-18 22:27:39 -0300

    main

--- chats/views/chats.py
+++ chats/views/chats.py
@@ -59,3 +59,34 @@ class ChatsView(BaseView):
         return Response({
             'chat': chat
         })
+
+class ChatView(BaseView):
+    def delete(self, request, chat_id):
+        #checking if chat belongs to user
+        chat = self.chat_belongs_to_user(
+            user_id=request.user.id,
+            chat_id=chat_id
+        )
+
+        # deleting chat
+        deleted = Chat.objects.filter(
+            id=chat_id,
+            deleted_at__isnull=True
+        ).update(
+            deleted_at=now()
+        )
+
+        if deleted:
+            #sending update chat to user
+               socket.emit('update_chat', {
+                   "type": "delete",
+                "query": {
+                    "chat_id": chat_id,
+                    "users": [chat.from_user_id, chat.to_user_id]
+                }
+            })
+               
+
+        return Response({
+            "success": True
+        })
