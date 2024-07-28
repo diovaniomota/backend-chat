2694a3be11be2e2f310fea1f728e06e4366d0ad2
Diovanio Mota <diovaniomota8@gmail.com>
2024-07-28 00:27:58 -0300

    main

--- attachments/serializers.py
+++ attachments/serializers.py
@@ -1,30 +1,26 @@
-from rest_framework import serializers
 
+from rest_framework import serializers
 from attachments.models import FileAttachment, AudioAttachments
 from attachments.utils.formatter import Formatter
-
 from django.conf import settings
 
-
-class FileAttachmentSerializer (serializers.ModelSerializer):
+class FileAttachmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = FileAttachment
         fields = '__all__'
 
-        def to_representation(self, instance):
-            data = super().to_representation(instance)
-            data['size'] = Formatter.format_bytes(instance.size)
-            data['src'] = f"{settings.CURRENT_URL}{instance.src}"
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['size'] = Formatter.format_bytes(instance.size)
+        data['src'] = f"{settings.CURRENT_URL}{instance.src}"
+        return data
 
-            return data
-        
-class AudioAttachmentSerializer (serializers.ModelSerializer):
+class AudioAttachmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = AudioAttachments
         fields = '__all__'
 
-        def to_representation(self, instance):
-            data = super().to_representation(instance)
-            data['src'] = f"{settings.CURRENT_URL}{instance.src}"
-
-            return data
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['src'] = f"{settings.CURRENT_URL}{instance.src}"
+        return data
